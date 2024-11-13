08c6a51e9ca35e5f0195dfa3f958b662f65f929b
YA811 <yousifahmedd21@gmail.com>
2024-11-13 13:44:28 +0300

    hw done

--- controllers/track.js
+++ controllers/track.js
@@ -58,3 +58,6 @@ router.delete('/:id', async (req, res) => {
       res.status(500).json({ error: error.message });
     }
 });
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,6 +3,9 @@ dotenv.config();
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const cors = require('cors');
+const methodOverride = require('method-override');
+
 
 //controllers file
 const trackRouter = require('./controllers/track');
@@ -14,12 +17,14 @@ mongoose.connection.on('connected', () => {
 });
 
 app.use(express.json());
+app.use(cors());
+app.use(methodOverride('_method'));  // for handling PUT and DELETE requests
 
 // track route
 app.use('/tracks', trackRouter);
 
 // Routes go here
 
-app.listen(3000, () => {
+app.listen(4000, () => {
   console.log('The express app is ready!');
 });
